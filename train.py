6e1f82258e1747f27f5ace0f79ba58bab75fa560
Mehmet Ozgur Turkoglu <ozgur.turkoglu@geod.baug.ethz.ch>
2023-01-02 18:00:28 +0100

    Update train.py

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def parse_args():
     All default values of these variables are those currently being used.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', "--data", type=str, default='../Preprocessing/S2_Raw_L2A_CH_2021_hdf5_train.hdf5', help="path to dataset")
+    parser.add_argument('-d', "--data", type=str, default='/cluster/work/igp_psr/tmehmet/S2_Raw_L2A_CH_2021_hdf5_train.hdf5', help="path to dataset")
     parser.add_argument('-b', "--batchsize", default=4, type=int, help="batch size")
     parser.add_argument('-w', "--workers", default=8, type=int, help="number of dataset worker threads")
     parser.add_argument('-e', "--epochs", default=30, type=int, help="epochs to train")
