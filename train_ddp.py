2356d490cd8f8a4dda31e0e2bb594abe04859d38
0zgur0 <ozgur.turkoglu@geod.baug.ethz.ch>
2023-01-04 15:46:50 +0100

    yy

--- train_ddp.py
+++ train_ddp.py
@@ -14,7 +14,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
 import torch.distributed as dist
 
-#xx
+#yy
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
