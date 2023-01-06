62a1a32a527a61a775d2e2d371a64800842ae739
0zgur0 <ozgur.turkoglu@geod.baug.ethz.ch>
2023-01-06 18:49:17 +0100

    s

--- read_hdf5.py
+++ read_hdf5.py
@@ -4,4 +4,9 @@ import h5py
 hf = h5py.File('/scratch2/tmehmet/swiss_crop/S2_Raw_L2A_CH_2021_hdf5_train.hdf5', 'r')
 print(hf.keys())
 
+#<KeysViewHDF5 ['data', 'gt', 'gt_canton', 'gt_instance']>
+
+print(data.shape)
+print(gt.shape)
+
 hf.close()
