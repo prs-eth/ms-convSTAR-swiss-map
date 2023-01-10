8ee9d3f16f9e9f47abda104a7c6a496de3b9fd78
0zgur0 <ozgur.turkoglu@geod.baug.ethz.ch>
2023-01-10 09:53:47 +0100

    s

--- train.py
+++ train.py
@@ -184,7 +184,7 @@ def main(
     if snapshot is not None:
         checkpoint = torch.load(snapshot)
         network.load_state_dict(checkpoint['network_state_dict'],, strict=False)
-        network_gt.load_state_dict(checkpoint['network_gt_state_dict'], , strict=False)
+        network_gt.load_state_dict(checkpoint['network_gt_state_dict'], strict=False)
         optimizer.load_state_dict(checkpoint['optimizerA_state_dict'])
         print("Model is loaded.")
 
