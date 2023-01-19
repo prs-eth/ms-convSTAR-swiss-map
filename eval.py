22ec7406e83af5845e263ac8a2a42a7d7374f0d5
0zgur0 <ozgur.turkoglu@geod.baug.ethz.ch>
2023-01-19 18:05:53 +0100

    s

--- eval.py
+++ eval.py
@@ -144,7 +144,7 @@ def evaluate_fieldwise(model, model_gt, dataset, batchsize=1, workers=1, viz=Fal
     if model_gt is not None:
         model_gt.eval()
 
-    dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batchsize, num_workers=workers, shuffle=True)
+    dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batchsize, num_workers=workers, shuffle=False)
 
     targets, logprobabilites_refined = test(model, model_gt, dataloader, level)
     # TODO TODO save the two probabilities for average mapping. prob map with max(logprob, 1). np.mean(5 prob distributions), np.sum. 
