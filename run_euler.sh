ddecac617d298a090fab54afe9749eea99b2c1ef
0zgur0 <ozgur.turkoglu@geod.baug.ethz.ch>
2023-02-15 16:49:15 +0100

    s

--- run_euler.sh
+++ run_euler.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --output /cluster/home/tmehmet/ms-convSTAR-swiss-map/euluer_outputs/outfile_%J.%I.txt
-#SBATCH --mem-per-cpu=13000
+#SBATCH --mem-per-cpu=240000
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:10g
 #SBATCH --mail-type=END,FAIL
